78d81d1f7df3fb4185e60f5bb12d9e87e6c5dae7
Seven4ME <nyanjasiek@gmail.com>
2018-10-17 16:29:04 +0300

    catch info about user department, from json file

--- server/config.js
+++ server/config.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const index = require('../index.js');
+const department = require('../jsonData/costumerDepartment.json');
 
 var app = express();
 
 app.set('port',process.env.PORT || 3000);
 
-
+app.get('/',function(req,res){
+    if(department.costumer == 1){
+        res.send('sip must redirect call to support');
+    }else if(department.costumer == 0){
+        res.send('sip must redirect call to collection');
+    }
+})
 
 app.use(function(req,res){
     res.type('text/plain');
